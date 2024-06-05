1d24f587091cfc0c76a5c6d0c53b81090538c583
josuerx12 <josueaze12@gmail.com>
2024-06-05 09:36:05 -0300

    feat: new features.

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bootstrap_1 = require("./server/bootstrap");
-const app = new bootstrap_1.Bootstrap();
-app.handle();
