30e38ef95cdb7d3764651d662a9f40e4bf9235c7
josuerx12 <josueaze12@gmail.com>
2024-06-05 10:09:22 -0300

    feat: new features;

--- src/config/wpp-instance.ts
+++ src/config/wpp-instance.ts
@@ -5,6 +5,7 @@ const wppInstance = new Client({
   puppeteer: {
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    //  executablePath: "/usr/bin/chromium-browser",
   },
   webVersionCache: {
     remotePath:
