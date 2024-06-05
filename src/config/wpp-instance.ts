53801f69ab0f6e541570ac1bf516f6c5a7a682b7
josuerx12 <josueaze12@gmail.com>
2024-06-05 09:32:54 -0300

    feat: new puppeter config.

--- src/config/wpp-instance.ts
+++ src/config/wpp-instance.ts
@@ -2,7 +2,10 @@ import { Client, LocalAuth } from "whatsapp-web.js";
 
 const wppInstance = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { headless: true },
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
   webVersionCache: {
     remotePath:
       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2411.2.html",
