d7315278ffbdd14f5094fd473c39788b6d488caa
josuerx12 <josueaze12@gmail.com>
2024-06-05 10:49:20 -0300

    feat: new features adjustment.

--- src/config/wpp-instance.ts
+++ src/config/wpp-instance.ts
@@ -4,12 +4,16 @@ const wppInstance = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--user-data-dir=puppeter/frotascap",
+    ],
     //  executablePath: "/usr/bin/chromium-browser",
   },
   webVersionCache: {
     remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2411.2.html",
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.51.html",
     type: "remote",
   },
 });
